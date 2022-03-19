1f4d35ff0c4d0839df4dd5d97dfe556fa4312c39
Arthur Schamroth <schamrotharthur@gmail.com>
2022-03-19 18:19:17 +0100

    Liaison backend/frontend ok

--- projet_frontend/src/App.js
+++ projet_frontend/src/App.js
@@ -1,11 +1,37 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [fichePatients, setFichePatients] = useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/fichePatient/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 32fd88f63f1e9f169ea9c09d9dd19d46ae7a2f4f'
+      }
+    })
+    .then(resp => resp.json())
+    // Permet de remplir le tableau fichePatients
+    .then(resp => setFichePatients(resp))
+    .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Allez le kiné</h1>
       </header>
+      <div className="layout">
+        <div>
+          {fichePatients.map(fichePatient => {
+            return <h2>{fichePatient.prenom + " " + fichePatient.nom}</h2>
+          })}
+        </div>
+        
+        <div>Détails patients</div>
+      </div>
     </div>
   );
 }
